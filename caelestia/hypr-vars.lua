7864391ec8d4d3cc794845c8140bd414b8044faa
Lim <Natsoons>
2026-08-27 01:36:44 +0700

    BackupHyprland

---
+++ caelestia/hypr-vars.lua
@@ -0,0 +1,19 @@
+return {
+    -- Default Apps --
+    browser = "zen-browser",
+    terminal = "kitty",
+    fileExplorer = "nautilus",
+    
+    -- Misc --
+    cursorTheme = "McMojave-cursors",
+    cursorSize = "32",
+    
+    ------------------
+    ---- KEYBINDS ----
+    ------------------
+    
+    -- Utilities --
+    kbScreenshot = "SUPER + SHIFT + S",
+    kbScreenshotFreeze = "Print",
+    
+}
