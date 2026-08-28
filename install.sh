6ffa825062aee3fc1aa2dbfe33b40843a75a7e8e
Natsoons <nicholassalim2006@gmail.com>
2026-08-29 01:05:22 +0700

    Menambahkan fungsi mengcopy-paste ke folder tujuan (buatan sendiri jelek)

--- install.sh
+++ install.sh
@@ -3,17 +3,22 @@
 # Tentukan direktori penyimpanan
 BACKUP_DIR="$HOME/BackupHyprland"
 CONFIG_DIR="$HOME/.config"
+PICTURES_DIR="$HOME/Pictures"
+VIDEOS_DIR="$HOME/Videos"
+WALL_DIR="$HOME/BackupHyprland/Wallpaper, GIF, dan lainnya"
 
 # Daftar folder yang akan dipulihkan (bisa ditambah nanti jika ada aplikasi lain)
 FOLDERS=("caelestia" "fastfetch")
+FOLDERS1=("Wallpapers" "Fetch")
+FOLDERS2=( "GIF" )
 
 echo "==========================================="
 echo "  Memulai Pemulihan Konfigurasi Sistem...  "
 echo "==========================================="
 
 # Cek apakah folder backup benar-benar ada
-if [ ! -d "$BACKUP_DIR" ]; then
-    echo "⚠️ Error: Folder $BACKUP_DIR tidak ditemukan!"
+if [ ! -d "$BACKUP_DIR" ] && [ ! -d "$WALL_DIR" ]; then
+    echo "⚠️ Error: Folder $BACKUP_DIR  dan Folder $WALL_DIR tidak ditemukan!"
     echo "Pastikan kamu sudah mengunduhnya dari GitHub."
     exit 1
 fi
@@ -30,8 +35,20 @@ for folder in "${FOLDERS[@]}"; do
     
     # Menyalin folder fisik dari repo hasil clone ke dalam sistem
     echo "    + Mengganti folder dengan yang baru..."
-    cp -r "$BACKUP_DIR/$folder"
+    cp -r "$BACKUP_DIR/$folder" "$CONFIG_DIR/$folder"
 
+done
+
+for folder1 in "${FOLDERS1[@]}"; do
+    echo "[*] Memindahkan folder $folder1"
+    
+    cp -r "$WALL_DIR/$folder1" "$PICTURES_DIR/"
+done
+
+for folder2 in "${FOLDERS2[@]}"; do
+    echo "[*] Memindahkan folder $folder2"
+    
+    cp -r "$WALL_DIR/$folder2" "$VIDEOS_DIR/"
 done
 
 echo "==========================================="
